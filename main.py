88cafc6c706ee5b973b44f1c5aba35796e1db38e
Redho Yu <monsterhunter172@gmail.com>
2024-01-22 21:47:35 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+
+def line_counter(path):
+    file_path = Path(path)
+    with open(file_path, 'r') as file:
+        line_count = sum(1 for x in file)
+    return line_count
+
+
+lines = line_counter('email_text.txt')
+print(lines)
